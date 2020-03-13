23ed9bb93d09314b86d301b0a8fe6b7435c5ecf4
Phonxay Champasouk <52714374+phonxaychampasouk@users.noreply.github.com>
2020-03-13 14:11:41 -0700

    Have configured DB to interact with HTTP request which will query seedDb Database

--- database/index.js
+++ database/index.js
@@ -5,7 +5,7 @@ const db = mongoose.connection;
 
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-  console.log('Connected to your umm what idk?');
+  console.log('Connected to mongoDb');
 });
 
 const imageSchema = new mongoose.Schema({
@@ -24,3 +24,4 @@ const imageSchema = new mongoose.Schema({
 const Image = mongoose.model('Image', imageSchema);
 
 module.exports = Image;
+module.exports = db;
--- server/index.js
+++ server/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+const db = require('../database/index.js');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/photos/:id', (req, res) => {
-  console.log("req: ", req.params.id);
-  res.send(req.params.id);
+  const reqId = parseInt(req.params.id, 10);
+  db.collection('images').find({ id: reqId }).toArray((err, result) => {
+    if (err) { throw err; } else {
+      res.send(result);
+    }
+  });
 });
 
 module.exports = app;
+
+
+// console.log('Req.params.id: ', typeof req.params.id) //string
